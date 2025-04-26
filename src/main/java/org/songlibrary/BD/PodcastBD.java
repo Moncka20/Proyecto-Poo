f01d713c2720966f0d0976e7dfaae2ae0de7a8b7
ThomasMoncayo <tamoncayo24@itp.edu.co>
2025-04-26 16:04:55 -0500

    septimo commit

--- src/main/java/org/songlibrary/BD/PodcastBD.java
+++ src/main/java/org/songlibrary/BD/PodcastBD.java
@@ -1,6 +1,7 @@
-package org.songlibrary.modelos;
+package org.songlibrary.BD;
 
 import java.util.ArrayList;
+import org.songlibrary.modelos.Podcast;
 
 public class PodcastBD {
     public static ArrayList<Podcast> podcasts = new ArrayList<>();
--- src/main/java/org/songlibrary/BD/ProductorMusicalBD.java
+++ src/main/java/org/songlibrary/BD/ProductorMusicalBD.java
@@ -1,6 +1,7 @@
-package org.songlibrary.modelos;
+package org.songlibrary.BD;
 
 import java.util.ArrayList;
+import org.songlibrary.modelos.ProductorMusical;
 
 public class ProductorMusicalBD {
     public static ArrayList<ProductorMusical> productores = new ArrayList<>();
